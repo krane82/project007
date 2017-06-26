992f0e3e01b822d4c253ddf104d0c4b02f00b334
viktor <krane82@gmail.com>
2017-06-26 14:36:47 +0300

    mironenko merged

--- test.php
+++ test.php
@@ -7,8 +7,8 @@ $infusionsoft = new \Infusionsoft\Infusionsoft(array(
     'redirectUri' => 'http://project008/test.php',
 ));
 //die();
-$t='O:18:"Infusionsoft\Token":4:{s:11:"accessToken";s:24:"zb9w5t25ccs2wa35auj6yx9u";s:12:"refreshToken";s:24:"shwc7nf9w5ncyc947qns9ess";s:9:"endOfLife";i:1495122716;s:9:"extraInfo";a:2:{s:10:"token_type";s:6:"bearer";s:5:"scope";s:27:"full|ki355.infusionsoft.com";}}';
-$_SESSION['token']=$t;
+//$t='O:18:"Infusionsoft\Token":4:{s:11:"accessToken";s:24:"zb9w5t25ccs2wa35auj6yx9u";s:12:"refreshToken";s:24:"shwc7nf9w5ncyc947qns9ess";s:9:"endOfLife";i:1495122716;s:9:"extraInfo";a:2:{s:10:"token_type";s:6:"bearer";s:5:"scope";s:27:"full|ki355.infusionsoft.com";}}';
+//$_SESSION['token']=$t;
 if (isset($_SESSION['token'])) {
     $infusionsoft->setToken(unserialize($_SESSION['token']));
 }
