c6d90cc406269aae6698ec5f145a135b4fcd86d1
Oleg <Ohrimus@yandex.ru>
2017-07-28 13:20:40 +0300

    added a markup a page email declininng reason for client

--- app/core/functions.php
+++ app/core/functions.php
@@ -1,32 +1,32 @@
 <?php
-function send_m($clientEmail, $p, $name, $tracking_number, $alttext = '',$linkToReject)
+function send_m($clientEmail, $p, $name, $tracking_number, $alttext = '', $linkToReject)
 {
-  $mail = new PHPMailer;
-  //  $mail->isSendmail();
-  $mail->IsSMTP(); // telling the class to use SMTP
-  $path = $_SERVER['DOCUMENT_ROOT'];
-  $path .= "/credentials.php";
-  require_once($path);
-  $mail->IsSMTP(); // telling the class to use SMTP
-  $mail->SMTPAuth   = true;                  // enable SMTP authentication
-  $mail->Host       = MAIL_HOST;    // sets the SMTP server
-  $mail->Port       = MAIL_PORT;
-  $mail->Username   = MAIL_USER;    // SMTP account username
-  $mail->Password   = MAIL_PASS;        // SMTP account password
+    $mail = new PHPMailer;
+    //  $mail->isSendmail();
+    $mail->IsSMTP(); // telling the class to use SMTP
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/credentials.php";
+    require_once($path);
+    $mail->IsSMTP(); // telling the class to use SMTP
+    $mail->SMTPAuth = true;                  // enable SMTP authentication
+    $mail->Host = MAIL_HOST;    // sets the SMTP server
+    $mail->Port = MAIL_PORT;
+    $mail->Username = MAIL_USER;    // SMTP account username
+    $mail->Password = MAIL_PASS;        // SMTP account password
 
-  $mail->isHTML(true);
+    $mail->isHTML(true);
 
-  $mail->AddReplyTo("leads@energysmart.com.au", "New Qualified Lead");
+    $mail->AddReplyTo("leads@energysmart.com.au", "New Qualified Lead");
 
-  $mail->SetFrom('leads@energysmart.com.au', 'New Qualified Lead');
+    $mail->SetFrom('leads@energysmart.com.au', 'New Qualified Lead');
 
-  $mail->AddAddress($clientEmail, $name);
+    $mail->AddAddress($clientEmail, $name);
 
-  $mail->Subject = "New Qualified Lead - Please contact ASAP";
+    $mail->Subject = "New Qualified Lead - Please contact ASAP";
 
-  $mail->AltBody = "To view the message, please use an HTML compatible email viewer!";  // optional, comment out and test
+    $mail->AltBody = "To view the message, please use an HTML compatible email viewer!";  // optional, comment out and test
 
-  $content = <<<EOD
+    $content = <<<EOD
   <body>
   <style type="text/css">
   tr: {
@@ -112,44 +112,21 @@ img[class="img-hide"] { max-width: 0px !important; max-height: 0px !important; }
 													<p> <multiline>We have a new qualified lead in your area! Please see their information below, and contact them as soon as possible.</multiline></p>
 EOD;
 
-    $content.='<div class="datagrid"><table style="width:100%;"><tbody>';
+    $content .= '<div class="datagrid"><table style="width:100%;"><tbody>';
     $i = true;
     foreach ($p as $v) {
-    /* $content .= '
 
-                                <table class="row tables-borders cell2"
-                                       style="border-collapse: collapse !important; border-spacing: 0 !important; display: table !important; position: relative !important; vertical-align: top; text-align: left; width: 100%; padding: 0; border: 1px ridge #636365;">
-                                  <tbody>
-                                  <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
-                                    <th class="small-5 large-4 columns first cell text-center right-border"
-                                        style="border-right-width: 1px; border-right-color: #636365; border-right-style: ridge; width: 33.33333%; text-align: center; color: #0a0a0a; font-family: Helvetica, Arial, sans-serif; font-weight: normal; line-height: 1.3; font-size: 16px; margin: 0 auto; padding: 10px 0 0px;"
-                                        align="center">
-                                      <p class="table-styles"
-                                         style="text-align: left; color: #0a0a0a; font-family: Helvetica, Arial, sans-serif; font-weight: normal; line-height: 1.3; font-size: 16px; margin: 0 0 10px; padding: 0 0 0 15px;"
-                                         align="left">' . $v["field_name"] . '</p>
-                                    </th>
-                                    <th class="small-7 large-8 columns last cell text-center"
-                                        style="width: 66.66667%; text-align: center; color: #0a0a0a; font-family: Helvetica, Arial, sans-serif; font-weight: normal; line-height: 1.3; font-size: 16px; margin: 0 auto; padding: 10px 0 0px;"
-                                        align="center">
-                                      <p class="table-styles"
-                                         style="text-align: left; color: #0a0a0a; font-family: Helvetica, Arial, sans-serif; font-weight: normal; line-height: 1.3; font-size: 16px; margin: 0 0 10px; padding: 0 0 0 15px;"
-                                         align="left">' . $v["val"] . '</p>
-                                    </th>
-                                  </tr>
-                                  </tbody>
-                                </table>';*/
-    if($i) {
-        $content .= '
+        if ($i) {
+            $content .= '
              <tr class="alt" ><td style="width:50%;"><strong>' . $v["field_name"] . '</strong></td><td style="width:50%;">' . $v["val"] . '</td></tr>';
+        } else {
+            $content .= ' <tr class="alt" style=" background-color:#F7F7F7;" ><td style="width:50%;"><strong>' . $v["field_name"] . '</strong></td><td style="width:50%;">' . $v["val"] . '</td></tr>';
+        }
+        $i = !$i;
     }
-    else {
-        $content .= ' <tr class="alt" style=" background-color:#F7F7F7;" ><td style="width:50%;"><strong>' . $v["field_name"] . '</strong></td><td style="width:50%;">' . $v["val"] . '</td></tr>';
-    }
-    $i = !$i;
-    }
-    $content.='</tbody></table></div>';
+    $content .= '</tbody></table></div>';
     $tracker = 'http://' . $_SERVER['HTTP_HOST'] . '/api/record?log=true&deliveryn=' . $tracking_number;
-    $content .= '<img alt="" src="'.$tracker.'" width="1" height="1" border="0" />';
+    $content .= '<img alt="" src="' . $tracker . '" width="1" height="1" border="0" />';
 
     $content .= <<<EOD
            <p>Please contact this lead immediately, as this customer is waiting to hear from you! If you can't get onto them, don't worry! Please attempt to call this customer at different times of the day (morning, day, and evening), as most people get home from work at 5:30-6pm! Be sure to make every effort to contact them.</p>
@@ -160,21 +137,18 @@ EOD;
 										  </tr>
  										  <!--button-->
   										  <tr>
-											  <td align="center">
-&nbsp;<table data-bgcolor="Button" bgcolor="#2991d6" border="0" align="center" cellpadding="0" cellspacing="0" style="border-radius:2px;">
-<tr>
- 																		<td data-link-style="text-decoration:none; color:#ffffff;" data-link-color="Button Link" data-size="Button" height="42" align="center" style="font-family: 'Open Sans', Arial, sans-serif; color:#FFFFFF; font-size:14px;font-weight: bold;letter-spacing: 1px;padding-left: 25px;padding-right: 25px;">
-																			<a href="http://leadpoint.energysmart.com.au"  style="text-decoration:none; color:#ffffff" data-color="Button Link">
-</a>
- 																			<singleline>
- 																			<a href="http://leadpoint.energysmart.com.au" style="text-decoration:none; color:#ffffff"> 	THIS  VIEW	LEAD IN YOUR PORTAL
- 				</a>																	</singleline>
- 																		</td>
- 																	</tr>
- 																</table>
-																 																	<p>&nbsp;</p>
-															</td>
+											  <td align="center">&nbsp;
+											      <table data-bgcolor="Button" bgcolor="#2991d6" border="0" align="center" cellpadding="0" cellspacing="0" style="border-radius:2px;">
+                                                        <tr>
+ 															<td data-link-style="text-decoration:none; color:#ffffff;" data-link-color="Button Link" data-size="Button" height="42" align="center" style="font-family: 'Open Sans', Arial, sans-serif; color:#FFFFFF; font-size:14px;font-weight: bold;letter-spacing: 1px;padding-left: 25px;padding-right: 25px;">
+																<a href="http://leadpoint.energysmart.com.au"  style="text-decoration:none; color:#ffffff" data-color="Button Link"></a>
+ 																    <singleline><a href="http://leadpoint.energysmart.com.au" style="text-decoration:none; color:#ffffff"> 	THIS  VIEW	LEAD IN YOUR PORTAL</a>																	</singleline>
+ 															</td>
  														</tr>
+ 												   </table>
+														    <p>&nbsp;</p>
+											  </td>
+ 										  </tr>
  														<!--end button-->
 									  </table>
 								  </td>
@@ -197,126 +171,127 @@ EOD;
   </body>          
 EOD;
 
-  $mail->msgHTML($content);
-  $emailSending = "";
-  if (!$mail->Send()) {
-    echo $emailSending = "Mailer Error: " . $mail->ErrorInfo;
-    return FALSE;
-  } else {
+    $mail->msgHTML($content);
+    $emailSending = "";
+    if (!$mail->Send()) {
+        echo $emailSending = "Mailer Error: " . $mail->ErrorInfo;
+        return FALSE;
+    } else {
 //    echo $emailSending = "Message sent!";
-    return TRUE;
-  }
+        return TRUE;
+    }
 }
 
 // check source, return ID
 function getCampaignID($source)
 {
-  $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  if ($con->connect_errno) {
-    printf("Connect failed: %s\n", $con->connect_error);
-    exit();
-  }
-  $sql = "SELECT * FROM `campaigns` WHERE source='$source'";
-  if ($result = $con->query($sql)) {
-    $id = $result->fetch_assoc();
-    $result->num_rows;
-    if($result) {
-      $con->close();
-      return $id["id"];
+    $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if ($con->connect_errno) {
+        printf("Connect failed: %s\n", $con->connect_error);
+        exit();
     }
-  }
-  $con->close();
-  return FALSE;
+    $sql = "SELECT * FROM `campaigns` WHERE source='$source'";
+    if ($result = $con->query($sql)) {
+        $id = $result->fetch_assoc();
+        $result->num_rows;
+        if ($result) {
+            $con->close();
+            return $id["id"];
+        }
+    }
+    $con->close();
+    return FALSE;
 }
 
 //phone number validation
 function phone_valid($phone)
 {
-  $justNums = preg_replace("/[^0-9]/", '', $phone);
-  return $justNums;
+    $justNums = preg_replace("/[^0-9]/", '', $phone);
+    return $justNums;
 }
 
 // only for Australian postcodes
 function postcodes_valid($postcode)
 {
-  $valid_post = preg_match("/^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$/", $postcode);
-  if($valid_post) {
-    return $postcode;
-  } else {
-    return FALSE;
-  }
-}
-
-
-function prepareLeadInfo($p){
-  $campaign_id = getCampaignID($p["source"]);
-  $readyLeadInfo = array();
-  $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  if ($con->connect_errno) {
-    printf("Connect failed: %s\n", $con->connect_error);
-    exit();
-  }
-
-  // query to get fields from campaign
-  $sql = 'SELECT lead_fields.id, lead_fields.key, lead_fields.name, campaign_lead_fields_rel.is_active, campaign_lead_fields_rel.mandatory';
-  $sql.= ' FROM `campaign_lead_fields_rel`';
-  $sql.= ' LEFT JOIN `lead_fields` ON lead_fields.id = campaign_lead_fields_rel.field_id';
-  $sql.= ' WHERE campaign_lead_fields_rel.campaign_id ='.$campaign_id;
-  $res = $con->query($sql);
-  if ($res) {
-    while( $res->fetch_assoc())
-    {
-      foreach ($res as $r) {
-        $key = $r["key"];
-        if($r["is_active"]){
-          $preArray["key"] = $key;
-          $preArray["val"] =  $p["$key"];
-          $preArray["field_name"] = $r["name"];
-          $readyLeadInfo[] = $preArray;
-          unset($preArray);
-        }
-      }
+    $valid_post = preg_match("/^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$/", $postcode);
+    if ($valid_post) {
+        return $postcode;
+    } else {
+        return FALSE;
     }
-  } else {
-    echo "<br>Something WRONG<br>";
-    return FALSE;
-  }
-  return $readyLeadInfo;
 }
 
-function formatReject($d){
-  switch ($d) {
-    case 0:
-      return "Reject accepted";
-      break;
-    case 1:
-      return "Approved";
-      break;
-    case 2:
-      return "Requested to Reject";
-      break;
-    case 3:
-      return "Reject not Approved";
-      break;
-    case 4:
-      return "Requested More info";
-    case 5:
-      return "";
-    default:
-      return "";
-  }
-}
 
-function redirect($location='/')
+function prepareLeadInfo($p)
 {
-  echo "<script>setTimeout(function() {
+    $campaign_id = getCampaignID($p["source"]);
+    $readyLeadInfo = array();
+    $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if ($con->connect_errno) {
+        printf("Connect failed: %s\n", $con->connect_error);
+        exit();
+    }
+
+    // query to get fields from campaign
+    $sql = 'SELECT lead_fields.id, lead_fields.key, lead_fields.name, campaign_lead_fields_rel.is_active, campaign_lead_fields_rel.mandatory';
+    $sql .= ' FROM `campaign_lead_fields_rel`';
+    $sql .= ' LEFT JOIN `lead_fields` ON lead_fields.id = campaign_lead_fields_rel.field_id';
+    $sql .= ' WHERE campaign_lead_fields_rel.campaign_id =' . $campaign_id;
+    $res = $con->query($sql);
+    if ($res) {
+        while ($res->fetch_assoc()) {
+            foreach ($res as $r) {
+                $key = $r["key"];
+                if ($r["is_active"]) {
+                    $preArray["key"] = $key;
+                    $preArray["val"] = $p["$key"];
+                    $preArray["field_name"] = $r["name"];
+                    $readyLeadInfo[] = $preArray;
+                    unset($preArray);
+                }
+            }
+        }
+    } else {
+        echo "<br>Something WRONG<br>";
+        return FALSE;
+    }
+    return $readyLeadInfo;
+}
+
+function formatReject($d)
+{
+    switch ($d) {
+        case 0:
+            return "Reject accepted";
+            break;
+        case 1:
+            return "Approved";
+            break;
+        case 2:
+            return "Requested to Reject";
+            break;
+        case 3:
+            return "Reject not Approved";
+            break;
+        case 4:
+            return "Requested More info";
+        case 5:
+            return "";
+        default:
+            return "";
+    }
+}
+
+function redirect($location = '/')
+{
+    echo "<script>setTimeout(function() {
   window.location.href = '$location';
 },5)</script>";
 }
 
 function dd($arg)
 {
-  echo "<pre>";
-  var_dump($arg);
-  die;
+    echo "<pre>";
+    var_dump($arg);
+    die;
 }
