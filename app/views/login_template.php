5d2ce9c427998497b5ec9c42feaba8bacbf5dd70
yaroslavkra <yaroslav.krasilchuk@jointoit.com>
2017-08-04 19:25:05 +0300

    uk design added

--- app/views/login_template.php
+++ app/views/login_template.php
@@ -1,58 +1,99 @@
 <!DOCTYPE html>
-<html lang="en-US">
-<head>
-	<title>Login | Energy Smart</title>
-	<meta content="width=device-width, initial-scale=1" name="viewport"/>
-	<meta charset="UTF-8">
-	<meta name="description" content="Energy Smart system" />
+<html class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title>Login | Energy Smart</title>
+    <meta content="Admin Dashboard" name="description">
+    <meta content="ThemeDesign" name="author">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<!-- Styles -->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
-	<link href="assets/plugins/pace-master/themes/blue/pace-theme-flash.css" rel="stylesheet"/>
-	<link href="assets/plugins/uniform/css/uniform.default.min.css" rel="stylesheet"/>
-	<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/plugins/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/plugins/line-icons/simple-line-icons.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/plugins/offcanvasmenueffects/css/menu_cornerbox.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/plugins/waves/waves.min.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/plugins/switchery/switchery.min.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/plugins/3d-bold-navigation/css/style.css" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-	<!-- Theme Styles -->
-	<link href="assets/css/modern.min.css" rel="stylesheet" type="text/css"/>
-	<link href="assets/css/themes/green.css" class="theme-color" rel="stylesheet" type="text/css"/>
-	<link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
-
-	<script src="assets/plugins/3d-bold-navigation/js/modernizr.js"></script>
-	<script src="assets/plugins/offcanvasmenueffects/js/snap.svg-min.js"></script>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="<?php echo __HOST__; ?>/assets/scss/style.css">
+    <link href="<?php echo __HOST__; ?>/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="//cdn.materialdesignicons.com/1.9.32/css/materialdesignicons.min.css">
 
 </head>
-<body class="<?php echo $body_class; ?>">
-<main class="page-content">
-	<div class="page-inner">
-		<div id="main-wrapper">
-	    <?php include 'app/views/'.$content_view; ?>
-		</div><!-- Main Wrapper -->
-	</div><!-- Page Inner -->
-</main><!-- Page Content -->
-<!-- Javascripts -->
-<script src="assets/plugins/jquery/jquery-2.1.4.min.js"></script>
-<script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
-<script src="assets/plugins/pace-master/pace.min.js"></script>
-<script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
-<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="assets/plugins/switchery/switchery.min.js"></script>
-<script src="assets/plugins/uniform/jquery.uniform.min.js"></script>
-<script src="assets/plugins/offcanvasmenueffects/js/classie.js"></script>
-<script src="assets/plugins/waves/waves.min.js"></script>
-<script src="assets/js/modern.min.js"></script>
-</body>
-</html>
+
+
+<body style="overflow: visible; background: #fafafa;">
+
+<!-- Begin page -->
+<div class="accountbg"></div>
+<div class="wrapper-page">
+
+    <div class="card">
+        <div class="card-block">
+
+            <h3 class="text-center mt-0 m-b-15">
+                <a href="index.html" class="logo logo-admin"><img src="<?php echo __HOST__; ?>/assets/images/logo.png" alt="logo"></a>
+            </h3>
+
+            <h4 class="text-muted text-center font-18"><b>Sign In</b></h4>
+
+            <div class="p-3">
+                <form class="form-horizontal m-t-20 m-t-md" action="" method="post">
+
+                    <div class="form-group row">
+                        <div class="col-12 spec-log-cl">
+                            <input class="form-control" type="text" name="login" placeholder="Email" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-12 spec-log-cl">
+                            <input class="form-control" type="password" name="password" placeholder="Password" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-12 spec-log-cl">
+                            <label class="custom-control custom-checkbox mb-2 mr-sm-2 mb-sm-0">
+                                <input type="checkbox" class="custom-control-input custom control indicator" id="rem" name="rem_sys">
+                                <span class="custom-control-description" style="font-weight:normal;">Remember me</span>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="form-group text-center row m-t-20">
+                        <div class="col-12 spec-log-cl">
+                            <button class="btn btn-block waves-effect waves-light spec-log-but" type="submit" style="color:white;">Log In</button>
+                        </div>
+                    </div>
+                </form>
+                <?php extract($data); ?>
+                <?php if($login_status=="access_granted") { ?>
+                    <p style="color:green">Success</p>
+                <?php } elseif($login_status=="access_denied") { ?>
+                    <p style="color:red" class="text-center">Wrong login or password</p>
+                <?php } ?>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+<!-- jQuery  -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/tether.min.js"></script><!-- Tether for Bootstrap -->
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/modernizr.min.js"></script>
+<script src="assets/js/detect.js"></script>
+<script src="assets/js/fastclick.js"></script>
+<script src="assets/js/jquery.slimscroll.js"></script>
+<script src="assets/js/jquery.blockUI.js"></script>
+<script src="assets/js/waves.js"></script>
+<script src="assets/js/jquery.nicescroll.js"></script>
+<script src="assets/js/jquery.scrollTo.min.js"></script>
+
+<!-- App js -->
+<script src="assets/js/app.js"></script>
+
+
+</body></html>
